243b8db28a805bc47d1769ec62cda958bd830473
PrathapDev7 <prathap.developer7@gmail.com>
2022-07-02 07:18:32 +0530

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,65 @@
+import "./navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+
+
+const Navbar = () => {
+  const { user , dispatch} = useContext(AuthContext);
+  const navigate = useNavigate();
+  var verify = 0;
+  try {
+    const admin = user.isAdmin;
+    if (admin ==1) {
+      verify += 1
+    }
+  }catch{}
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGOUT" });
+    try {
+      await axios.post("http://localhost:8000/api/auth/logout");
+      navigate("/")
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">House Inn</span>
+        </Link>
+        
+        {verify == 1 ? 
+        <button className="admin">Dashboard</button> 
+        : <></>}
+
+        {user ? <div className="logout">
+        <Link to="/seller">
+        <button className="partner">Add your Property?</button>
+        </Link>
+        &nbsp;&nbsp;Hello,&nbsp;
+        {user.username}
+        
+            <button onClick={handleClick} className="navButton">Logout</button>
+          </div>
+           : 
+           (
+          <div className="navItems">
+            <Link to="/register">
+            <button className="navButton">Register</button>
+            </Link>
+            <Link to="/login">
+            <button className="navButton">Login</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
